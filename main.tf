340ace00266e1f92a4ba7a525b65aac12353138f
JorgeFR85 <jorgonzalez@gmail.com>
2024-10-03 09:57:37 +0200

    E

--- main.tf
+++ main.tf
@@ -1,9 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.3.0"  # Recomendación para utilizar una versión estable y actualizada
+    }
+  }
+}
+
 provider "azurerm" {
   features {} # Habilita las características predeterminadas del proveedor
+
   subscription_id = var.subscription_id
   client_id       = var.client_id
   client_secret   = var.client_secret
   tenant_id       = var.tenant_id
+
 }
 
 # Se crea el resource group
